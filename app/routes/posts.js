79269c02560e0303c54ab6dc9c76148284da3714
harshawije97 <harshawije99@gmail.com>
2026-01-28 20:25:48 +0530

    final commit

---
+++ app/routes/posts.js
@@ -0,0 +1,45 @@
+import { Router } from "express";
+const Posts = Router();
+
+let data = [
+  {
+    id: 1,
+    post: "Hello World is th most overused wording in the programming",
+    description: "This is a description of the post",
+  },
+  {
+    id: 2,
+    post: "Post 2",
+    description: "This is a description of the post 2",
+  },
+  {
+    id: 3,
+    post: "Post 3",
+    description: "This is a description of the post 3",
+  },
+];
+
+Posts.get("/", (req, res) => {
+  const { search_post } = req.query;
+
+  if (search_post) {
+    const filteredPosts = data.filter((post) =>
+      post.post.toLowerCase().includes(search_post.toLowerCase()),
+    );
+    return res.json(filteredPosts);
+  }
+  res.json(data);
+});
+
+Posts.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const getPost = data.find((post) => post.id === id);
+
+  if (!getPost) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+
+  res.status(200).json(getPost);
+});
+
+export default Posts;
--- server.js
+++ server.js
@@ -1,27 +1,15 @@
-const express = require("express");
-const path = require("path");
+"use strict";
+
+import express, { static as _static } from "express";
+import { join } from "path";
+import Posts from "./app/routes/posts.js";
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-
-let data = [
-  {
-    id: 1,
-    post: "Hello World is th most overused wording in the programming",
-    description: "This is a description of the post",
-  },
-  {
-    id: 2,
-    post: "Post 2",
-    description: "This is a description of the post 2",
-  },
-  {
-    id: 3,
-    post: "Post 3",
-    description: "This is a description of the post 3",
-  },
-];
+// Body parser middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Routes -- root api
 /**
@@ -35,28 +23,11 @@ app.get("/about", (req, res) => {
  */
 
 // Static files
-app.use(express.static(path.join(__dirname, "public")));
+// ! There is a error in this static file setup. Uncomment this to find the answer later
+// app.use(_static(join(__dirname, "public")));
 
-app.get("/api/posts", (req, res) => {
-  const {search_post} = req.query;
-
-  if(search_post) {
-    const filteredPosts = data.filter(post => post.post.toLowerCase().includes(search_post.toLowerCase()));
-    return res.json(filteredPosts);
-  }
-  res.json(data);
-});
-
-app.get("/api/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const getPost = data.find((post) => post.id === id);
-
-  if (!getPost) {
-    return res.status(404).json({ message: "Post not found" });
-  }
-
-  res.status(200).json(getPost);
-});
+// API Routes
+app.use("/api/v1/posts", Posts);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
